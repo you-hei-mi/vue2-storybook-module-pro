b792aa9d77ee1db4e3ddcb85a6086b751b169b39
邹华 <zouh@kaiyufound.com>
2023-09-07 17:25:31 +0800

    docs: 打包静态文件

---
+++ storybook-static/sb-addons/storybook-10/manager-bundle.js
@@ -0,0 +1,3 @@
+try{
+var d=__STORYBOOKADDONS__,{addons:t,types:p,mockChannel:O}=__STORYBOOKADDONS__;var y=__STORYBOOKTHEMING__,{CacheProvider:S,ClassNames:b,Global:u,ThemeProvider:x,background:k,color:C,convert:N,create:a,createCache:R,createGlobal:B,createReset:D,css:G,darken:K,ensure:Y,ignoreSsrWarning:v,isPropValid:P,jsx:A,keyframes:E,lighten:H,styled:I,themes:M,typography:j,useTheme:w,withTheme:V}=__STORYBOOKTHEMING__;var s=a({base:"light",brandTitle:"storybook-pro"});t.setConfig({theme:s});
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
